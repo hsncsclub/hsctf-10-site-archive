fa60619d0a5df5cd2a0b11284ce15ff9d6480232
Larry Xue <larry.p.xue@gmail.com>
2023-06-18 01:18:09 -0400

    initial

---
+++ uploads/6c6f5f4b2f9b27af9fbee72095a4483a17d58af8eba9f5fe0bd512d51d9f7f2f/broken_calculator.py
@@ -0,0 +1,53 @@
+flag = #############################################################################
+
+def f(x):
+	s = str(x)
+	k = len(s)//2
+	return "#"*(len(s)-k) + s[k:]
+
+print("Welcome to the Half-Calculator")
+print("Operations:")
+print("    * x : multiplies by x")
+print("    / x : divides by x")
+print("    ++ : adds one")
+print("    -- : subtracts one")
+print()
+print("printing variable ... FLAG")
+print()
+print(f(flag))
+print()
+
+try:
+	i = 0
+	while i < 6:
+		x = input("Operation: ")
+		l = x.split()
+		if len(l) < 1: 
+			print("Invalid input")
+		elif l[0] == "*":
+			u = int(l[1])
+			flag *= u
+			print(f(flag))
+			i += 1
+		elif l[0] == "/":
+			u = int(l[1])
+			if flag % u == 0:
+				flag //= u
+				print(f(flag))
+				i += 1
+			else:
+				print("ERROR: divisibility check failed")
+		elif l[0] == "++":
+			flag += 1
+			print(f(flag))
+			i += 1
+		elif l[0] == "--":
+			flag -= 1
+			print(f(flag))
+			i += 1
+		else:
+			print("Invalid input")
+		print()
+	print("ERROR: LOW BATTERY")
+except:
+	print("unknown error")
